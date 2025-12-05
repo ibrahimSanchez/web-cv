5819ad840fa6567080605236db70d43f47750179
Ibrahim Marquez Sanchez <sanchezibrahim296@gmail.com>
2025-12-05 04:55:49 -0500

    Preparando deploy sin backend

--- src/components/layouts/admin/MainAdmin.tsx
+++ src/components/layouts/admin/MainAdmin.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import { useProjectsStore } from "@/src/hooks/store/useProjectsStore";
-import { useEffect } from "react";
-import { Button } from "../../ui/button";
 
 export function MainAdmin(){
 
-
-    const { projects, isLoading, fetchProjects } = useProjectsStore();
-    console.log(projects);
-    useEffect(() => {
-        fetchProjects();
-    }, []);
     return <div className="pt-32">
-     
-     <Button onClick={fetchProjects}>Fetch Projects</Button>
-
+        hola
     </div>;
 }
--- src/lib/test-db.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "./prisma";
-
-async function test() {
-  const count = await prisma.project.count();
-  console.log("Projects in DB:", count);
-}
-
-test();
